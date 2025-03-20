8780e5b5a99406ee8fa6eaa7906392f4acce8bc3
CalebTesfaye <155718351+CalebTesfaye@users.noreply.github.com>
2025-03-20 17:43:42 +0300

    Add files via upload

---
+++ str.title().py
@@ -0,0 +1,9 @@
+text5 = "  leading and trailing spaces  "
+title_text5 = text5.title()
+print(title_text5)
+#output:   Leading And Trailing Spaces
+
+text4 = "it's a nice day"
+title_text4 = text4.title()
+print(text4.title())
+#output: It'S A Nice Day
