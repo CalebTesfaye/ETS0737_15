c882561b32898534897a18e5baf54e02233e120d
CalebTesfaye <155718351+CalebTesfaye@users.noreply.github.com>
2025-03-23 22:42:42 +0300

    Add files via upload

---
+++ str.capitalize().py
@@ -0,0 +1,7 @@
+word = "hello"
+capitalized_word = word.capitalize()
+print(capitalized_word)  # Output: Hello
+
+sentence = "this is a sentence."
+capitalized_sentence = sentence.capitalize()
+print(capitalized_sentence)  # Output: This is a sentence.
