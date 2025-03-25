d09096bc55ac64c300670df55752a8ffcb0583cd
CalebTesfaye <155718351+CalebTesfaye@users.noreply.github.com>
2025-03-25 23:11:52 +0300

    Add files via upload

---
+++ str.index().py
@@ -0,0 +1,7 @@
+#Find "o" starting from index 5
+index_of_o = text.index("o", 5)
+print(f"Index of 'o' starting from 5: {index_of_o}")  # Output: 8
+
+#Find the index of "world"
+index_of_world = text.index("world")
+print(f"Index of 'world': {index_of_world}")  # Output: 7
