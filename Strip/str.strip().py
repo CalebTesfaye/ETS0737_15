e6c308549cc5c226021e2dccc31a8653f16ed606
CalebTesfaye <155718351+CalebTesfaye@users.noreply.github.com>
2025-03-27 02:19:16 +0300

    Add files via upload

---
+++ Strip/str.strip().py
@@ -0,0 +1,7 @@
+text = ",,,hello world!!!"
+stripped_text = text.strip(",!")
+print(stripped_text)  # Output: hello world
+
+url = "https://www.kalebtesfaye.com/"
+stripped_url = url.strip("/")
+print(stripped_url) #output: https://www.kalebtesfaye.com
