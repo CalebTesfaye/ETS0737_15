13ca287c673f017988f1a60f33f0ada04268c1dd
CalebTesfaye <155718351+CalebTesfaye@users.noreply.github.com>
2025-03-27 02:18:39 +0300

    Add files via upload

---
+++ Replace/str.replace().py
@@ -0,0 +1,7 @@
+text = "I like apples and apples."
+new_text = text.replace("apples", "oranges")
+print(new_text)  # Output: I like oranges and oranges.
+
+text = "This string has extra spaces."
+new_text = text.replace(" ", "")
+print(new_text)  # Output: Thisstringhasextraspaces.
