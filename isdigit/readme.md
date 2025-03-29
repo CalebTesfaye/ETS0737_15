00c3e4ec1d393dd505afca23e031f51addaea832
CalebTesfaye <155718351+CalebTesfaye@users.noreply.github.com>
2025-03-29 23:39:35 +0300

    Add files via upload

---
+++ isdigit/readme.md
@@ -0,0 +1,1 @@
+The isdigit() method returns True if all characters in the string are digits (0-9) and there is at least one character. It returns False otherwise.
---
+++ isdigit/str.isdigit().py
@@ -0,0 +1,11 @@
+# String containing only digits
+number1 = "12345"
+print(number1.isdigit())  # Output: True
+
+# String containing letters
+number2 = "abcde"
+print(number2.isdigit())  # Output: False
+
+# String containing special characters
+number3 = "!@#$%"
+print(number3.isdigit())  # Output: False
