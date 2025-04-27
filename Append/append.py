d383ec504186a5ccb57ad823629672be4e490ed5
CalebTesfaye <155718351+CalebTesfaye@users.noreply.github.com>
2025-04-27 03:13:06 +0300

    Add files via upload

---
+++ Append/append.py
@@ -0,0 +1,5 @@
+Food = ["eggs, milk", "bread", "fruit"]
+Food.append("vegetables")
+print(Food)
+# Output: ['eggs, milk', 'bread', 'fruit', 'vegetables']
+# The append() method adds an item to the end of the list.
---
+++ Copy/copy.py
@@ -0,0 +1,4 @@
+Top_list = ["apple", "banana", "cherry"]
+Top_list2 = Top_list.copy()
+print(Top_list2)    
+# Output: ['apple', 'banana', 'cherry']
